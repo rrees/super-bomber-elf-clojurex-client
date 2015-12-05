5aa9f27a4c7522684f8ab0ba08de3ff081bef070
Robert Rees <robert.rees@guardian.co.uk>
2015-12-05 07:40:51 +0000

    Creates some helper functions for interacting via the REPL

--- src/super_bomber_elf_client/core.clj
+++ src/super_bomber_elf_client/core.clj
@@ -1,7 +1,27 @@
 (ns super-bomber-elf-client.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.data.json :as json]
+      [gniazdo.core :as ws]))
+
+(def connection-url "ws://178.62.75.118:80")
+
+(def show-messages (atom false))
+
+(defn messages [] (swap! show-messages (fn [a] (not a))))
+
+(defn create-socket [] (ws/connect connection-url
+  :on-receive (fn receive-message [message] (if @show-messages (println (json/read-str message))))))
+
+(defn send-command [socket command]
+    (ws/send-msg socket (json/write-str command)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (messages)
+    (let [ socket (create-socket)]
+        (send-command socket {"command" ["SetName" "Robert"]})
+        (ws/close socket))
+  )
+
+;;{validCommands [{command DropBomb} {command [SetName <name>]} {command MoveNorth} {command MoveSouth} {command MoveEast} {command MoveWest}], hint Messages must be valid JSON, and plain strings aren't allowed as top-level JSON elements, so everything must be wrapped in a message object. Blame Doug Crockford, not me!}
